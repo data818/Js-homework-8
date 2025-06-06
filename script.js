4dc167216236712e5a4e43b68e0a9bfdf433449d
Davit kvarelashvili <kvarelashvili18@gmail.com>
2025-06-06 21:56:41 +0400

    improvements
    adding png

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const openBtn = document.querySelector("#popUpBtnOn");
-const closeBtn = document.querySelector("#popupBtnOff");
-const popup = document.querySelector("#popup");
+const openButton = document.querySelector("#popUpBtnOn");
+const hidebutton = document.querySelector("#popupBtnOff");
+const Popup = document.querySelector("#popup");
 
 document.querySelector("#popUpBtnOn").addEventListener("click", function () {
   document.querySelector("#popup").style.display = "flex";

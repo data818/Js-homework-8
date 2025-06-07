d8f3258587b0b21bac81018284b95a76a269ab02
Davit kvarelashvili <kvarelashvili18@gmail.com>
2025-06-07 16:28:55 +0400

    Popup/function
    adding popup on/of function

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const openButton = document.querySelector("#popUpBtnOn");
 const hidebutton = document.querySelector("#popupBtnOff");
 const Popup = document.querySelector("#popup");
 
+document.querySelector("#popupBtnOff").addEventListener("click", function () {
+  document.querySelector("#popup").style.display = "none";
+});
+
 document.querySelector("#popUpBtnOn").addEventListener("click", function () {
   document.querySelector("#popup").style.display = "flex";
 });
